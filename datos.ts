44eceecc2b9ab6bf158a8262b60389d94291d623
CintiaAlvarez <104107300+CintiaAlvarez@users.noreply.github.com>
2022-04-29 06:28:08 -0300

    Initial commit

---
+++ datos.ts
@@ -0,0 +1,11 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+// let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  //el código va acá
+  console.log("el dato ingresado es ", dato.value);
+  //)
+});
+
+rotulo.innerHTML = "Ingresar dato...";
